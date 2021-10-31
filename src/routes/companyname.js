8cfe94c1b7f69fdaa91b2b01c4a7bb550ae6c41e
AlubuArt <jc@jcvisueldesign.dk>
2021-10-31 09:38:51 +0100

    added calls to virk api

--- src/routes/companyname.js
+++ src/routes/companyname.js
@@ -1,10 +1,43 @@
 const express = require('express');
 const router = express.Router();
+const axios = require('axios');
 
 //GET companyname
-router.get('/', function(req, res) {
-    console.log('Companyname route');
-    res.render('index', {message: 'Hallo Companyname route'})
-});
-
-module.exports = router;
+router.get("/", async (req, res) => {
+    let param = req.query.input;
+  
+    let data = JSON.stringify({
+      _source: ["Vrvirksomhed.virksomhedMetadata"],
+      query: {
+          query_string:{
+            default_field:"Vrvirksomhed.virksomhedMetadata.nyesteNavn.navn",
+            query: `${param}`
+          },
+      },
+      size: 1
+    });
+  
+    let config = {
+      method: "POST",
+      url: "http://distribution.virk.dk/cvr-permanent/virksomhed/_search\n",
+      headers: {
+          'Authorization': 'Basic ' + process.env.VIRK_KEY,
+          "Content-Type": "application/json; charset=UTF-8",
+          
+      },
+      data: data,
+    };
+  
+    axios(config)
+      .then((response) => {
+        console.log(JSON.stringify(response.data));
+        console.log(response.data);
+        res.send(response.data);
+      })
+      .catch((error) => {
+        console.log(error);
+      });
+  });
+  
+  
+  module.exports = router;
--- src/routes/cvrnr.js
+++ src/routes/cvrnr.js
@@ -1,10 +1,45 @@
-const express = require('express');
+const express = require("express");
 const router = express.Router();
+const axios = require("axios");
 
 //GET cvrnr
-router.get('/', function(req, res) {
-    console.log('CVRnr route');
-    res.render('index', {message: 'Hallo Cvrnr route'})
-})
+router.get("/", async (req, res) => {
+  console.log("CVRnr route");
+
+  let param = req.query.input;
+
+  let data = JSON.stringify({
+    _source: ["Vrvirksomhed.virksomhedMetadata"],
+    query: {
+      term: {
+        "Vrvirksomhed.cvrNummer": `${param}`,
+      },
+    },
+  });
+
+  let config = {
+    method: 'POST',
+    url: 'http://distribution.virk.dk/cvr-permanent/virksomhed/_search\n',
+    headers: { 
+      'Authorization': 'Basic ' + process.env.VIRK_KEY, 
+      'Content-Type': 'application/json; charset=UTF-8'
+    },
+    data : data
+  };
+  
+  axios(config)
+  .then((response) => {
+    console.log(JSON.stringify(response.data));
+    console.log(response.data)
+    res.send(response.data)
+    
+    
+  })
+  .catch((error) => {
+    console.log(error);
+  });
+  
+});
+
 
 module.exports = router;
